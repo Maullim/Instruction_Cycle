b468639f1499d4233d621420062a1c64a4496c13
suyong <buildmaru19@gmail.com>
2026-04-10 17:48:37 +0900

    16진수 출력은 0x 추가, 십진수 출력은 std::dec을 붙여서 명확하게 명시

--- main.cpp
+++ main.cpp
@@ -11,10 +11,10 @@ int main()
     std::cout << "Programming Project #1 Instruction Cycle 구현\n14조(이수용, 이세언, 이소민, 이수현)\n\n\n";
     // cpu 객체 만들기
     Cpu myCpu;
-    // myCpu.init();
+    myCpu.init();
 
     // 구현한 모든 sample로 test
-    myCpu.test_init();
+    // myCpu.test_init();
     while (myCpu.isRunning())
     {
         myCpu.fetch();
